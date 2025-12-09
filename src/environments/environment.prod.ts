d3b083e4901333ef375bd40deb2d4d51786697d2
Jhonatan171 <jhoanatanguevaravasquez@gmail.com>
2025-12-08 23:27:48 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // ESTE ES EL VALOR QUE RENDER REEMPLAZARÁ con la URL real de tu backend
-  API_BASE_URL: 'REPLACE_ME_BY_RENDER_URL'
+  API_BASE_URL: 'https://tenbi-backend.onrender.com'
 };
